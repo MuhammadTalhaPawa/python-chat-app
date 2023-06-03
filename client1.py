66ac12ebb7ad013c7e81e0bb70e8512b8542ccff
MuhammadTalhaPawa <133965264+MuhammadTalhaPawa@users.noreply.github.com>
2023-06-04 00:06:34 +0500

    test made two clients which only sends data to the server

--- client1.py
+++ client1.py
@@ -1,14 +1,4 @@
 import socket
-import threading
-
-def receive_messages(client_socket):
-    while True:
-        message = client_socket.recv(1024).decode()
-        if not message:
-            break
-        print("Received from server:", message)
-
-    client_socket.close()
 
 def run_client():
     host = 'localhost'
@@ -17,53 +7,13 @@ def run_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
 
-    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
-    receive_thread.start()
-
     while True:
-        message = input("Enter your message: ")
+        message = input("Enter message: ")
         client_socket.send(message.encode())
 
-        if message.lower() == "exit":
+        if message.lower() == "exit chat":
             break
 
     client_socket.close()
 
 run_client()
-
-
-# import socket
-
-# def run_client():
-#     host = 'localhost'
-#     port = 8888
-
-#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     client_socket.connect((host, port))
-
-#     print('in run client')
-
-
-# run_client()
-
-# # import socket
-
-# # def run_client():
-# #     host = 'localhost'
-# #     port = 8888
-
-# #     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# #     client_socket.connect((host, port))
-
-# #     while True:
-# #         message = input("Enter message: ")
-# #         client_socket.send(message.encode())
-# #         response = client_socket.recv(1024).decode()
-# #         print("Server response:", response)
-
-# #         if message.lower() == "exit":
-# #             break
-
-# #     client_socket.close()
-
-# # run_client()
--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 import socket
 import threading
 
+debug_exit = False
+
 def handle_client(client_socket, client_address):
     while True:
         message = client_socket.recv(1024).decode()
+        if message == 'exit server':
+            debug_exit = True
         if not message:
             break
-        # print(f"Received from {client_address}: {message}")
+        print(f"Received from {client_address}: {message}")
         # Forward the message to the other client
-        other_client = clients[1] if client_socket == clients[0] else clients[0]
-        other_client.send(message.encode())
+        # other_client = clients[1] if client_socket == clients[0] else clients[0]
+        # other_client.send(message.encode())
 
     client_socket.close()
 
@@ -23,12 +27,7 @@ def run_server():
     print("Server listening on {}:{}".format(host, port))
 
     while True:
-        # is_client_first_connected = False
-        for client in clients:
-            print(client)
-
-        a = input("Enter Code:");
-        if(a == "exit"):
+        if debug_exit:
             break
         client_socket, addr = server_socket.accept()
         print("Connected with", addr)
@@ -44,77 +43,3 @@ clients = []
 run_server()
 
 
-
-# import socket
-# import threading
-
-# def handle_client(client_socket):
-#     while True:
-#         message = client_socket.recv(1024).decode()
-#         if not message:
-#             break
-#         print("Received message:", message)
-#         response = input("Enter your response: ")
-#         client_socket.send(response.encode())
-
-#     client_socket.close()
-
-# def run_server():
-#     host = 'localhost'
-#     port = 8888
-
-#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     server_socket.bind((host, port))
-#     server_socket.listen(5)
-#     print("Server listening on {}:{}".format(host, port))
-
-#     while True:
-#         # if(len(clien))
-#         for client in clients:
-#             print(client)
-#         client_socket, addr = server_socket.accept()
-#         print("Connected with", addr)
-#         clients.append(client_socket)
-#         client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-#         client_thread.start()
-
-
-# clients = []
-
-# run_server()
-
-
-
-# # import socket
-# # import threading
-
-# # def handle_client(client_socket):
-# #     while True:
-# #         message = client_socket.recv(1024).decode()
-
-# #     # while True:
-# #     #     message = client_socket.recv(1024).decode()
-# #     #     if not message:
-# #     #         break
-# #     #     print("Received message:", message)
-# #     #     response = input("Enter your response: ")
-# #     #     client_socket.send(response.encode())
-
-# #     client_socket.close()
-
-# # def run_server():
-# #     host = 'localhost'
-# #     port = 8888
-
-# #     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# #     server_socket.bind((host, port))
-# #     server_socket.listen(5)
-# #     print("Server listening on {}:{}".format(host, port))
-
-# #     while True:
-# #         client_socket, addr = server_socket.accept()
-# #         print("Connected with", addr)
-# #         client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-# #         client_thread.start()
-
-# # run_server()
